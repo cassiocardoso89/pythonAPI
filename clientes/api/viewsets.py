4e74b59c1ac326cf021f9e05de30a935a84c8b42
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-01-29 13:16:47 -0300

    feat: validações aplicadas

--- clientes/api/viewsets.py
+++ clientes/api/viewsets.py
@@ -13,46 +13,62 @@ class ClientViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         cep = request.data.get('cep')
-        response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+        if not cep:
+            return Response({"message": "CEP não informado"}, status=Response.status_code)
+        try:
+            response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            return Response({"menssagem": str(error)}, status=Response.status_code)
+        
         address_data = response.json()
+        
         serializer.is_valid(raise_exception=True)
         serializer.validated_data.update({
             'cidade': address_data['localidade'],
             'estado': address_data['uf'],
             'logradouro': address_data['logradouro']
         })
+        
         serializer.is_valid(raise_exception=True)
+        
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=response.status_code, headers=headers)
+        return Response(serializer.data, status=Response.status_code, headers=headers)
         
     def get_queryset(self):
         queryset = self.queryset
-        cpf = self.request.query_params.get('cpf', None)
-        cep = self.request.query_params.get('cep', None)
-        nome = self.request.query_params.get('nome', None)
+        cpf = self.request.query_params.get('cpf')
+        cep = self.request.query_params.get('cep')
+        nome = self.request.query_params.get('nome')
 
-        if cpf is not None:
+        if cpf:
             queryset = queryset.filter(cpf=cpf)
-        if cep is not None:
+        if cep:
             queryset = queryset.filter(cep=cep)
-        if nome is not None:
+        if nome:
             queryset = queryset.filter(nome__icontains=nome)
 
         return queryset
 
     def delete_queryset(self, request, *args, **kwargs):
-        client = self.get_object()
-        client.delete()
-        return Response({"message": "Client deleted successfully"})
+        try:
+            client = self.get_object()
+            client.delete()
+            return Response({"menssagem": "Cliente removido com sucesso"})
+        except Exception as e:
+            return Response({"erro": str(e)}, status=Response.status_code)
 
     def update_queryset(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+        if not serializer.is_valid():
+            return Response({"erro": serializer.errors}, status=Response.status_code)
+        try:
+            self.perform_update(serializer)
+        except Exception as e:
+            return Response({"error": str(e)}, status=Response.status_code)
+        return Response(serializer.data, status=Response.status_code)
 
     def perform_update(self, serializer):
-     serializer.save()
-
+        serializer.save()
