19ef14b7f5073ede38400e494bbf0f35b3a49666
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-01-29 13:38:49 -0300

    fix: campo telefone validando qtd de caracteres

--- clientes/models.py
+++ clientes/models.py
@@ -6,13 +6,16 @@ def validate_cpf(value):
     if len(value) != 11:
         raise ValidationErr("CPF deve conter 11 dígitos.")
 
+def validate_telefone(value):
+    if len(value) != 11:
+        raise ValidationErr("Telefone deve conter 11 dígitos.")
+
 class Clientes(models.Model):
     id_cliente = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome = models.CharField(max_length=255)
     cpf = models.CharField(max_length=11, validators=[validate_cpf])
-    telefone =  models.IntegerField()
+    telefone = models.CharField(max_length=11, validators=[validate_telefone])
     genero =  models.CharField(max_length=10)
     cep =  models.IntegerField()
     cidade =  models.CharField(max_length=255, editable=False)
     estado =  models.CharField(max_length=255, editable=False)
-    logradouro =  models.CharField(max_length=255, editable=False)
